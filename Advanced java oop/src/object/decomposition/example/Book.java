1f75ec7d5f8529d2e20f2ff900c1527c787b10b8
Sourav <sourav.iit.nstu@gmail.com>
2022-05-12 12:10:26 +0600

    book review example

--- Advanced java oop/src/object/decomposition/example/Book.java
+++ Advanced java oop/src/object/decomposition/example/Book.java
@@ -1,26 +1,30 @@
 package object.decomposition.example;
 
+import java.util.ArrayList;
+
 public class Book {
 
     // state
     private int id;
     private String name;
     private String author;
-    private Review review;
+    private ArrayList<Review> reviews = new ArrayList<Review>();
 
 
-    Book(int id,String name,String author, Review review){
+    Book(int id,String name,String author){
         this.id = id;
         this.name = name;
         this.author = author;
-        this.review = review;
+
+    }
+
+    public void addReview(Review review){
+        this.reviews.add(review);
 
     }
 
     public String toString(){
-        return String.format("Id : %d\n Name : %s\n Author : %s\n Review : %s",id,name,author,review);
+        return String.format("Id : %d\n Name : %s\n Author : %s\n Review : %s",id,name,author,reviews);
     }
-
-
 
 }
--- Advanced java oop/src/object/decomposition/example/Main.java
+++ Advanced java oop/src/object/decomposition/example/Main.java
@@ -3,9 +3,9 @@ package object.decomposition.example;
 public class Main {
     public static void main(String[] args) {
 
-        Review review = new Review(10,"Great Book",5);
-        Book book = new Book(123,"OOP","XYZ",review);
-
+        Book book = new Book(123,"OOP","XYZ");
+        book.addReview(new Review(10,"Great Book",5));
+        book.addReview(new Review(101,"nice Book",4));
         System.out.println(book);
     }
 }
