a6b550e736032eed2675fbf2906f52f1d37191ca
Sourav <sourav.iit.nstu@gmail.com>
2022-05-12 13:17:43 +0600

    lambda expression

---
+++ Advanced java oop/src/basic/oop/interfaceExample/Telusko.java
@@ -0,0 +1,39 @@
+package basic.oop.interfaceExample;
+
+/*
+   3 types of interface
+
+   1. Marker interface -> without any method
+   2. SAM - > Single abstract method || Functional interface
+   3. Normal
+ */
+
+import java.sql.SQLOutput;
+
+interface ABC{
+
+    // every method in interface is public abstract
+    // we can not create an instance of an interface
+
+    public void show();
+}
+
+//class ABCImp implements ABC{
+//
+//
+//    @Override
+//    public void show() {
+//        System.out.println("in show");
+//    }
+//}
+
+public class Telusko {
+    public static void main(String[] args) {
+//      ABC obj = new ABCImp();
+//      obj.show();
+        ABC obj = ()->{
+            System.out.println("in show");
+        };
+        obj.show();
+    }
+}
