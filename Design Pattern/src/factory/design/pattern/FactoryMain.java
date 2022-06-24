eba8ca2604a46a9fda0ff48bf39b5baf67681fea
Sourav <sourav.iit.nstu@gmail.com>
2022-06-24 18:11:00 +0600

    builder pattern

--- Design Pattern/src/factory/design/pattern/FactoryMain.java
+++ Design Pattern/src/factory/design/pattern/FactoryMain.java
@@ -3,6 +3,9 @@ package factory.design.pattern;
 public class FactoryMain {
     public static void main(String[] args) {
 
+        // hide the implementation of classes
+        // factory returns the instance of the class
+
         OperatingSystemFactory operatingSystemFactory = new OperatingSystemFactory();
         OS obj = operatingSystemFactory.getInstance("closed");
         obj.spec();
